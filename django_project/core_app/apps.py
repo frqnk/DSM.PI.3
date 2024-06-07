2ebddd754514271e5359004361804862c7faebe9
Isabela Almeida <128005941+isabalmeida@users.noreply.github.com>
2024-06-06 22:24:09 -0300

    Delete django_project directory

--- django_project/core_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core_app'
--- django_project/core_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views as v
-
-urlpatterns = [
-    path('', v.home_view, name='home'),
-    path('login/', v.login, name='login'),
-    path('logout/', v.logout, name='logout'),
-    path('signup', v.signup, name='signup'),
-]
